9570897b9981443ae51eb5a5909b3b80f3e2871f
anirbanjana883 <167280484+anirbanjana883@users.noreply.github.com>
2025-07-05 13:42:32 +0530

    Update first.js

--- bgackground-color/first.js
+++ bgackground-color/first.js
@@ -1,48 +1,6 @@
-// approach 1 :
-
-// const red=document.getElementById('red');
-// const blue=document.getElementById('blue');
-// const orange=document.getElementById('orange');
-// const green=document.getElementById('green');
-// const purple=document.getElementById('purple');
-// // access of the body 
-// const body=document.querySelector('body');
-
-// red.addEventListener('click',()=>{
-//     body.style.backgroundColor='red';
-// });
-
-// blue.addEventListener('click',()=>{
-//     body.style.backgroundColor='blue';
-// });
-
-// orange.addEventListener('click',()=>{
-//     body.style.backgroundColor='orange';
-// });
-
-// green.addEventListener('click',()=>{
-//     body.style.backgroundColor='green';
-// });
-
-// purple.addEventListener('click',()=>{
-//     body.style.backgroundColor='purple';
-// });
 
 
-// approach 2 :
-
-// const buttons =document.querySelectorAll('button');
-// const body = document.body;
-
-// buttons.forEach((button)=>{
-//     button.addEventListener('click',()=>{
-//         // console.log(button.id); // we are getting each button id 
-//         body.style.backgroundColor=button.id;
-//     });
-// });
-
-
-// approach 3: event bubbling 
+// approach : event bubbling 
 
 const root=document.getElementById("root");
 root.addEventListener('click',(event)=>{
